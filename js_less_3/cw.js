609f5a1493d80c2d02a40d3aa891fe1bf6ec07c9
Dimas <zhuravel16@gmail.com>
2021-11-26 19:34:04 +0200

    less 3

--- js_less_3/cw.js
+++ js_less_3/cw.js
@@ -11,21 +11,94 @@ let arr = [2,17,13,6,22,31,45,66,100,-18];
 //             console.log(`${arr[i]}`)
 //         }
 //     3. перебрати циклом while та вивести  числа тільки з непарним індексом
-        let i =0;
-        while (i < arr.length){
-            if (i % 2!==0){
-                console.log(num)
-                i++
-            }
-        }
+//         let i = 0;
+//         while (i < arr.length){
+//             if (i % 2 !== 0){
+//                 console.log(arr[i]);
+//             }
+//             i++;
+//         }
 // 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+//     for (i =0; i < arr.length; i++){
+//         const inarr = arr[i];
+//         if (inarr % 2 !== 0){
+//             console.log(inarr);
+//         }
+//     }
 // 5. перебрати циклом while та вивести  числа тільки парні  значення
+//                 let i = 0;
+//                 while (i < arr.length){
+//                     if (i % 2 === 0){
+//                         console.log(arr[i]);
+//                     }
+//                     i++;
+//                 }
 // 6. перебрати циклом for та вивести  числа тільки парні  значення
+//             for (i =0; i < arr.length; i++){
+//                 const inarr = arr[i];
+//                 if (inarr % 2 === 0){
+//                     console.log(inarr);
+//                 }
+//             }
 // 7. замінити кожне число кратне 3 на слово "okten"
+
+// підглянув вирішення у відео практика
+ 
+        // for (item of arr){
+        //     if (item % 3 === 0){
+        //         item = "okten";
+        //         document.write(item + " ")
+        //     }
+        // }
 // 8. вивести масив в зворотньому порядку.
+//             for (let i = arr.length - 1; i >= 0; i--) {
+//                 let item = arr[i];
+//                 if (item % 3 === 0){
+//                     item = "okten";
+//                     document.write(item + " ")
+//                 }
+//             }
 // 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+//   1.перебрати його циклом while
+//           let i = arr.length - 1;
+//            while (i>=0){
+//                console.log(arr[i])
+//            i--
+//         };
 
 
+    // 2. перебрати його циклом for
+            // for (let i = arr.length - 1; i>=0; i--){
+            //     console.log(arr[i])
+            // }
 
-
-
+//     3. перебрати циклом while та вивести  числа тільки з непарним індексом
+//         let i = arr.length - 1;
+//         while (i>=0){
+//             if (i % 2 !== 0){
+//                 console.log(arr[i]);
+//             }
+//             i--;
+//         }
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+//     for (i = arr.length - 1; i>=0; i--){
+//         let inarr = arr[i];
+//         if (inarr % 2 !== 0){
+//             console.log(inarr);
+//         }
+//     }
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+//                 let i = arr.length -1;
+//                 while (i>=0){
+//                     if (i % 2 === 0){
+//                         console.log(arr[i]);
+//                     }
+//                     i--;
+//                 }
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+//             for (i = arr.length - 1; i>=0; i--){
+//                 let inarr = arr[i];
+//                 if (inarr % 2 === 0){
+//                     console.log(inarr);
+//                 }
+//             }
--- js_less_3/hm.js
+++ js_less_3/hm.js
@@ -3,8 +3,13 @@
 // - з 5 стічкових значень
 // - з 5 значень стрічкового, числового та булевого типу
 // - та вивести його в консоль
-//     let array = [1,45,225,-4,6.3];
-//     console.log(array)
+//     let arraynum = [1,45,225,-4,6.3];
+//     console.log(arraynum);
+//     console.log(arraynum[0]);
+//     console.log(arraynum[1]);
+//     console.log(arraynum[2]);
+//     console.log(arraynum[3]);
+//     console.log(arraynum[4]);
 //     let arraystring = ['party','book','js','green','apple'];
 //     console.log(arraystring)
 //     let arraymix = ['program', 33, true,'octen',-45];
@@ -12,10 +17,12 @@
 
 // -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
 //     let arr = [];
-//     for (let i = 0; i < 3; i++){
-//         console.log(arr[i]= [1,45,'sadg'])
-//     }
-
+//         arr[0] = '1';
+//         arr[1] = -44;
+//         arr[2] = 'octen';
+//         arr[3] = 325;
+//     console.log(arr);
+//     console.log(arr[2]);
 
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 //     for (let a = 0; a <10; a++) {
@@ -32,14 +39,14 @@
 //             h++
 //         }
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-//             let h = 1;
+//             let h = 0;
 //             while (h <= 20){
-//                 document.write(`<h1>text${h}</h1>`)
+//                 document.write(`<h1>text${h+1}</h1>`)
 //                 h++
 //             }
 // - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
 //     let arr = [3,64,7,89,2,54,6,89,-43,3];
-//     for (let i = 0; i < 10; i++){
+//     for (let i = 0; i < arr.length; i++){
 //         console.log(`${arr[i]}`)
 //     }
 // - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
@@ -89,15 +96,26 @@
 
 
 // - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-            for (i = 0; i <= 100; i++){
-                    console.log(i)
-                    document.write(i)
-            }
+//              for (i = 1; i < 100; i++){
+//                 console.log(i)
+//                 document.write(`${i}`)
+//             }
 // - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
-            for (i = 1; i <= 200; i++){
-                console.log(`${i}`)
-                document.write(`${i}`)
-            }
+//             for (i = 1; i < 100; i = i + 2){
+//                 console.log(i)
+//                 document.write(`${i}`)
+//             }
 // - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
-
+//             for (i =0; i < 100; i++){
+//                 if (i % 2 === 0){
+//                     console.log(i);
+//                     document.write(`${i}`)
+//                 }
+//             }
 // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+//                 for (i =0; i < 100; i++){
+//                     if (i % 2 !== 0){
+//                         console.log(i);
+//                         document.write(`${i}`)
+//                     }
+//                 }
